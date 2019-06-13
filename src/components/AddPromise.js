cf7eed5d2dbd79a9876544ad033d00c1e402dc9c
Pedro Terriquez <pedroterriquez@hotmail.com>
2019-06-13 13:49:23 -0500

    Edit PaymentPromise

--- src/components/AddPromise.js
+++ src/components/AddPromise.js
@@ -7,30 +7,54 @@ export default class AddPromise extends Component {
   constructor(props){
     super(props);
     this.state = {
+      id: null,
       period_quantity: 10,
-      period_amount: 10,
       period: 1,
       title: '',
       interest: '',
-      amount: '',
       total: '',
     }
     this.newPromise = this.newPromise.bind(this);
   }
 
-  newPromise() {
-    values = {
+  componentDidMount() {
+    this.setState({ ...this.props.navigation.getParam('props') })
+  }
+
+  promiseValues() {
+    return values = {
       payment_period_quantity: this.state.period_quantity,
-      title: this.state.title,
-      amount_payments: this.state.period_amount,
       payment_period: this.state.period,
+      title: this.state.title,
       interest: this.state.interest,
       total: this.state.total,
-      //recipient_id: this.props.navigation.getParam('id')
+      recipient_id: this.props.navigation.getParam('id')
     }
-    axios.post(`${global.API_URL}/payment_promises/`, values, deviceStorage.loadToken() )
+  }
+
+  handleSave() {
+    var id = this.props.navigation.getParam('props').id
+    if( id != null) {
+      this.updatePromise(id)
+    } else {
+      this.newPromise()
+    }
+  }
+
+  updatePromise(id) {
+    axios.patch(`${global.API_URL}/payment_promises/${id}`, this.promiseValues(), deviceStorage.loadToken() )
       .then((response) => {
-        console.log("PROMISE CREATED")
+        this.props.navigation.goBack()
+      })
+      .catch((error)=>{
+        console.log(error);
+      })
+  }
+
+  newPromise() {
+    axios.post(`${global.API_URL}/payment_promises/`, this.promiseValues() , deviceStorage.loadToken() )
+      .then((response) => {
+        this.props.navigation.goBack()
       })
       .catch((error)=>{
         console.log(error);
@@ -40,7 +64,9 @@ export default class AddPromise extends Component {
   render() {
     return(
       <View>
+        <Text>Period: </Text>
         <Picker
+          placeholder="Select a period"
           selectedValue={this.state.period}
           onValueChange={(itemValue, itemIndex) => this.setState({period: itemValue})
           }>
@@ -48,29 +74,39 @@ export default class AddPromise extends Component {
           <Picker.Item label="Weekly" value="week" />
           <Picker.Item label="Monthly" value="month" />
       </Picker>
-        <TextInput
-          placeholder='Times to pay'
-          onChangeText={ (text) => this.setState({period_quantity: text}) }
-          keyboardType={ 'numeric' }
-        />
-        <TextInput
-          placeholder='Concept'
-          onChangeText={ (text) => this.setState({title: text}) }
-        />
-        <TextInput
-          placeholder='Interest'
-          onChangeText={ (text) => this.setState({title: text}) }
-          keyboardType={ 'numeric' }
-        />
-        <TextInput
-          placeholder='Amount'
-          onChangeText={ (text) => this.setState({amount: text}) }
-          keyboardType={ 'numeric' }
-        />
-        <Button
-          title='Send'
-          onPress={ () => this.newPromise() }
-        />
+      <Text>Money per payment</Text>
+      <TextInput
+        placeholder='Money per payment'
+        value={ this.state.period_quantity.toString() }
+        onChangeText={ (text) => this.setState({period_quantity: text}) }
+        keyboardType={ 'numeric' }
+      />
+      <Text>Concept</Text>
+      <TextInput
+        placeholder='Concept'
+        value={ this.state.title }
+        onChangeText={ (text) => this.setState({title: text}) }
+      />
+      <Text>Interest</Text>
+      <TextInput
+        placeholder='Interest'
+        value={ this.state.interest }
+        onChangeText={ (text) => this.setState({interest: text}) }
+        keyboardType={ 'numeric' }
+      />
+      <Text>Total finance</Text>
+      <TextInput
+        placeholder='Total finance'
+        value={ this.state.total }
+        onChangeText={ (text) => this.setState({total: text}) }
+        keyboardType={ 'numeric' }
+      />
+      <Text>Payment Period Quantity</Text>
+      <Text value={ this.state.total/this.state.period_quantity }/>
+      <Button
+        title='Save'
+        onPress={ () => this.handleSave() }
+      />
       </View>
     )
   }
--- src/components/presentational/PromiseHeader.js
+++ src/components/presentational/PromiseHeader.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
-import { Text, TextInput, View } from 'react-native';
+import { Text, Button, View } from 'react-native';
+import { withNavigation } from 'react-navigation';
 
-export default class PromiseHeader extends Component {
+class PromiseHeader extends Component {
   render() {
-    const { id, title, paid, total } = this.props
+    const { id, title, paid, total, navigation } = this.props
     return(
       <View>
         <Text>Id: { id }</Text>
         <Text>Title: { title }</Text>
         <Text>Paid: { paid }</Text>
         <Text>Total: { total }</Text>
+        <Button title='Edit' onPress={()=> navigation.navigate('AddPromise', { props: this.props }) }/>
       </View>
     )
   }
 }
+
+export default withNavigation(PromiseHeader)
